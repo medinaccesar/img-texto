ecff0282f36deff7fcd257d5359ae4bc1f758eef
CésarM <medinaccesar@mlogicial.com>
2023-11-01 14:47:34 +0100

    Se añade gui...

--- services/img_texto.py
+++ services/img_texto.py
@@ -1,3 +1,4 @@
+import re
 from PIL import Image
 import pytesseract
 
@@ -9,13 +10,17 @@ class ImgTexto():
             # Se trata de una ruta a la imagen, se abre
             imagen = Image.open(imagen)
         text = pytesseract.image_to_string(imagen)        
-        return text
+        return self.limpiar_texto(text)
     
-    def extraer_parte(image_path, crop_rectangle):
+    def extraer_parte(self,image_path, crop_rectangle):
         image = Image.open(image_path)
         cropped_image = image.crop(crop_rectangle)
         text = pytesseract.image_to_string(cropped_image)
-        return text
+        return self.limpiar_texto(text)
     
     def es_imagen(self,obj):
         return isinstance(obj, Image.Image)
+    
+    def limpiar_texto(self,text):        
+        limpio = re.sub(r'[]', '', text)
+        return limpio
