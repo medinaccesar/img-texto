ec363ac71cf6f0b40f255f8a2e2e4e037ff3d7ee
CésarM <medinaccesar@mlogicial.com>
2023-11-05 20:59:31 +0100

    Se añaden nuevas constantes

--- constantes.py
+++ constantes.py
@@ -5,13 +5,14 @@ class Configuracion:
     __slots__ = ()
     NOMBRE_AP = 'img-texto'
     DESCRIPCION_AP = ' Extractor simple del texto de una imagen'
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
     CREDITOS = 'César Medina'
     
    
     
     # Directorio de la aplicación
-    DIR_APP = os.path.dirname(os.path.abspath(__file__))   
+    DIR_APP = os.path.dirname(os.path.abspath(__file__))  
+    DIR_DOCUMENTOS = os.path.expanduser("~") 
     DIR_IMA = 'rec'+os.path.sep  
     DIR_SALIDA = 'salida'+os.path.sep  
     DIR_TXT = DIR_IMA
